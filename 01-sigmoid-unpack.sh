bd1f3abfd1eb9d786928b9f6e2559eae22a547f9
0x6d696368 <>
2019-10-01 00:54:32 +0200

    Added https://www.npcglib.org/~stathis/blog/precompiled-openssl/

---
+++ 01-sigmoid-unpack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+libpath="$(pwd)/lib"
+
+provider="sigmoid"
+
+cd ${provider}
+ls | while read f; do
+	if echo "${f}" | grep "\.tar\.gz$"; then
+		dir=$(echo ${f} | sed 's/\.tar.gz//g')
+		mkdir -p "${dir}"
+		tar -xf "${f}" -C "${dir}"
+	elif echo "${f}" | grep "\.zip$"; then
+		dir=$(echo ${f} | sed 's/\.zip//g')
+		mkdir -p "${dir}"
+		unzip -o "${f}" -d "${dir}"
+	elif echo "${f}" | grep "\.7z$"; then
+		dir=$(echo ${f} | sed 's/\.7z//g')
+		mkdir -p "${dir}"
+		7z -y x "${f}" -o"${dir}"
+	else
+		continue
+	fi
+	lib=$(echo "${dir}" | sed 's/\([^-]\+\)-\([^-]\+\)-\([^-]\+\)\..\+/\1/g')
+	ver=$(echo "${dir}" | sed 's/\([^-]\+\)-\([^-]\+\)-\([^-]\+\)\..\+/\2/g')
+	var=$(echo "${dir}" | sed 's/\([^-]\+\)-\([^-]\+\)-\([^-]\+\)\..\+/\3/g')
+	path="${libpath}/${provider}/${lib}/${ver}/${var}"
+	mkdir -p "${path}"
+
+	cd "${dir}"
+	find -type f -not -iname '*.lib' -and -not -iname '*.a' -exec rm {} \;
+	cp -R . "${path}/."
+	cd ..
+done
+cd ..
+
